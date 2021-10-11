4cbe19e474c8570788860ad6b4b64312cabfccc6
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 22:40:47 +0700

    Update val.py

--- val.py
+++ val.py
@@ -4,10 +4,11 @@ from nn.nets import network
 from utils.data_loader import validation_examples, validation_targets 
 
 
-def validation(weight_path=None, X=None, y=None)
+def validation(weight_path=None, X=None, y=None):
   model = tflearn.DNN(network)
   model.load(weight_path)
   y_pred = model.predict(X)
   return accuracy_score(y_true, y_pred)
 
-print(f'Accuracy score of the validation dataset: {validation(weight_path='model.tflearn', X=validation_examples, y=validation_targets)}')
+accuracy = validation(weight_path='save/model.tflearn', X=validation_examples, y=validation_targets)
+print(f'Accuracy score of the validation dataset: {accuracy}')
