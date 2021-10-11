2e09e2727d22da3edbb8f10d8788eb7cb9285a02
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 21:49:56 +0700

    Update nets.py

--- nn/nets.py
+++ nn/nets.py
@@ -1,10 +1,10 @@
 import tflearn
 
-def network()
+def network():
   # Build neural network
   net = tflearn.input_data(shape=[None, 1])
   net = tflearn.fully_connected(net, 32)
   net = tflearn.fully_connected(net, 32)
-  net = tflearn.fully_connected(net, 2, activation='softmax')
+  net = tflearn.fully_connected(net, 1, activation='softmax')
   net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
   return net
