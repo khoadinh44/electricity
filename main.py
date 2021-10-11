81c183a53ce12437af9f756bf639e9f9e9003b8f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-12 02:09:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 import numpy as np
 import tflearn
 
-from utils.data_loader import training_examples, training_targets
+from utils.data_loader import training_examples, training_targets, validation_examples, validation_targets
 from nn.nets import network
 
 # active GPU
 tf.debugging.set_log_device_placement(True)
 
 # Load model------------------------------------------------------------------------------------
-def train(data=None, labels=None, network=network, num_epochs=10, batch_size=32, show_metric=True, path_saver=None):
+def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=10, batch_size=32, show_metric=True, name_saver=None):
   model = tflearn.DNN(network(), tensorboard_dir='/content/electricity/save/tflearn_logs')
-  model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric)
-#   model.save('/content/electricity/save/'+path_saver)
+  model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric, validation_set=(val_data, val_labels))
+  model.save('/content/electricity/save/'+name_saver)
 
-train(data=training_examples, labels=training_targets, network=network, path_saver='/content/electricity/save/model.tfl')
+train(training_examples, training_targets, network=network, name_saver='model.tflearn')
