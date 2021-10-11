840a87d78e6579d50cc57e63eb0454cd837d71ab
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 22:39:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import tflearn
@@ -5,12 +6,11 @@ import tflearn
 from utils.data_loader import training_examples, training_targets
 from nn.nets import network
 
-print(training_examples.shape, training_targets.shape)
 # active GPU
-# tf.debugging.set_log_device_placement(True)
+tf.debugging.set_log_device_placement(True)
 
 # Load model------------------------------------------------------------------------------------
-def train(data=None, labels=None, network=network, num_epochs=50, batch_size=32, show_metric=True, path_saver='electricity/save/'):
+def train(data=None, labels=None, network=network, num_epochs=10, batch_size=32, show_metric=True, path_saver='electricity/save/'):
   model = tflearn.DNN(network())
   model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric)
   model.save(path_saver)
