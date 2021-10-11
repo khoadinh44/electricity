35720137211d04dc8859cb0d39c3016413defe5e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 22:48:16 +0700

    Update val.py

--- val.py
+++ val.py
@@ -2,13 +2,12 @@ import tensorflow as tf
 import tflearn
 from nn.nets import network
 from utils.data_loader import validation_examples, validation_targets 
+from sklearn.metrics import accuracy_score
 
-
-def validation(weight_path=None, X=None, y=None):
-  model = tflearn.DNN(network)
+def validation(weight_path=None, X=None, y_true=None):
+  model = tflearn.DNN(network())
   model.load(weight_path)
   y_pred = model.predict(X)
   return accuracy_score(y_true, y_pred)
-
-accuracy = validation(weight_path='save/model.tflearn', X=validation_examples, y=validation_targets)
+accuracy = validation('/content/electricity/save/model.tfl', validation_examples, validation_targets)
 print(f'Accuracy score of the validation dataset: {accuracy}')
