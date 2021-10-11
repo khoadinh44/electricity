d522915302905cbcd2111bfedcdef33bc3eb79e8
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 21:06:15 +0700

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
+import numpy as np
 
 # Function to define the inputs. Different depending on the model and turbine
 def preprocess_features(wind_farm_dataframe):
     selected_features = wind_farm_dataframe[["WSpeed_1"]] 
-    return selected_features
+    return np.array(selected_features)
 
 
 def preprocess_targets(wind_farm_dataframe):  
 # Function to define the target of the neuralnetwork. “Power_1” changes depending on the wind turbine
     selected_targets = wind_farm_dataframe[["Power_1"]]
-    return selected_targets
+    return np.array(selected_targets)
 
 # Function used to construct the columns used by the program with the data
 def construct_feature_columns(input_features):
