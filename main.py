0f62ba164ad2663453b6d252bc6e65adf1d75b61
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-11 21:50:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import tensorflow as tf
 import numpy as np
 import tflearn
 
-from utils import training_examples, training_targets
-from nets.nn import network
+from utils.data_loader import training_examples, training_targets
+from nn.nets import network
 
+print(training_examples.shape, training_targets.shape)
 # active GPU
-tf.debugging.set_log_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
 
 # Load model------------------------------------------------------------------------------------
-def train(data=None, labels=None, network=network, num_epochs=50, batch_size=32, show_metric=True, path_saver='electricity/save/')
-  model = tflearn.DNN(network)
-  model.fit(data, labels, num_epochs=num_epochs, batch_size=batch_size, show_metric=show_metric)
+def train(data=None, labels=None, network=network, num_epochs=50, batch_size=32, show_metric=True, path_saver='electricity/save/'):
+  model = tflearn.DNN(network())
+  model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric)
   model.save(path_saver)
 
-train(data=training_examples, labels=training_targets, network=network)
+train(data=training_examples, labels=training_targets, network=network, path_saver='electricity/save/')
