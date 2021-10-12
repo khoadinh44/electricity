61e18eff7afd0a42e9c48a39bd93380076df0ec1
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-12 13:36:05 +0700

    Update nets.py

--- nn/nets.py
+++ nn/nets.py
@@ -1,4 +1,5 @@
 import tflearn
+from utils.optimizers import rmsprop, adam, adagrad, momentum
 
 def network():
   # Build neural network
@@ -6,5 +7,5 @@ def network():
   net = tflearn.fully_connected(net, 64)
   net = tflearn.fully_connected(net, 32)
   net = tflearn.fully_connected(net, 2, activation='softmax')
-  net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+  net = tflearn.regression(net, optimizer=momentum, loss='categorical_crossentropy')
   return net
