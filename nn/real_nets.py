f7f9d347d9e7fbf3c621d9cbe9d78cc142eeab73
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-13 16:55:31 +0700

    Update real_nets.py

--- nn/real_nets.py
+++ nn/real_nets.py
@@ -1,7 +1,6 @@
 import tflearn
 import tensorflow as tf
 from utils.optimizers import rmsprop, adam, adagrad, momentum
-
 def leaky_relu(z, name=None):
   return tf.maximum(0.01 * z, z, name=name)
 
@@ -9,6 +8,7 @@ loss_obj = tf.keras.losses.CategoricalCrossentropy(reduction=tf.keras.losses.Red
 
 def network():
   # Build neural network
+  net = tflearn.input_data(shape=[None, 4])
   net = tflearn.fully_connected(net, 32)
   net = tf.keras.layers.PReLU()(net)
   net = tflearn.fully_connected(net, 16)
