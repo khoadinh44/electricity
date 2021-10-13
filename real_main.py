f41a1cb95c8644d059e285ba7378a2e9555b57a9
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-13 15:52:42 +0700

    Update real_main.py

--- real_main.py
+++ real_main.py
@@ -1,50 +1,20 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-from numpy import genfromtxt
+import tflearn
 
-data_path='data/real.XLS'
-num_val=10
-num_data=508
-# Function to define the inputs. Different depending on the model and turbine
-def preprocess_features(wind_farm_dataframe):
-    selected_features = wind_farm_dataframe[1:num_data, 0:4]
-    return np.array(selected_features)
+from utils.data_real_loader import training_examples, training_targets, validation_examples, validation_targets
+from nn.real_nets import network
+# active GPU
+tf.debugging.set_log_device_placement(True)
 
+num_epochs = 200
+batch_size = 64
 
-def preprocess_targets(wind_farm_dataframe):  
-    selected_targets = wind_farm_dataframe[1:num_data, 52:56]
-    return np.array(selected_targets)
+# Load model------------------------------------------------------------------------------------
+def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=num_epochs, batch_size=batch_size, show_metric=True, name_saver=None):
+  model = tflearn.DNN(network(), tensorboard_dir='/content/electricity/save/tflearn_logs')
+  model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric, validation_set=(val_data, val_labels))
+  model.save('/content/electricity/save/'+name_saver)
 
-# Function used to construct the columns used by the program with the data
-def construct_feature_columns(input_features):
-    return set([tf.feature_column.numeric_column(my_feature) for my_feature in input_features])
-
-# Function used to load dataset
-def input_fn(features, labels, training=True, batch_size=16, num_epochs=1):
-        """An input function for training or evaluating"""
-        # Convert the inputs to a Dataset.
-        dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
-
-        # Shuffle and repeat if you are in training mode.
-        if training:
-            dataset = dataset.shuffle(1000).repeat()
-        dataset = dataset.batch(batch_size).repeat(num_epochs)
-        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-        return dataset
-
-# Prepare data------------------------------------------------------------------------------------
-wind_farm_dataframe = np.array(genfromtxt(data_path, delimiter='\t'))
-examples = preprocess_features(wind_farm_dataframe)
-targets = preprocess_targets(wind_farm_dataframe)
-all_data = np.concatenate((examples, targets), axis=-1).astype(np.float32)
-val_indices = np.random.choice(all_data.shape[0], size=num_val, replace=False)
-train_indices = [i for i in range(len(all_data)) if i not in val_indices]
-
-all_train = all_data[train_indices]
-training_examples = all_train[:, :4]
-training_targets = all_train[:, 4:]
-
-all_validation = all_data[val_indices]
-validation_examples = all_validation[:, :4]
-validation_targets = all_validation[:, 4:]
+train(training_examples, training_targets, validation_examples, validation_targets, network=network, name_saver='model.tflearn')
