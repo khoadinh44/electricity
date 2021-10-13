5ecd3b3c5da86f67f0fc0e73fcc8edc7fe8f968e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-13 16:56:38 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -4,7 +4,16 @@ from nn.real_nets import network
 import numpy as np
 
 print('\nEnter WSpeed: ')
-inputs = np.array([[0.5, 0.7, 1.1, 1.2]])
+inputs = np.array([[ 7.9,  8.9,  8.9,  9.3],
+ [10.6, 11.5,  7.8, 10.7],
+ [ 3.9,  4.6,  3.9,  3.6],
+ [ 2.,   1.9,  1.7,  0.8],
+ [11.3, 12.6, 10.2, 11.7],
+ [ 2.9,  2.5,  1.5,  1.9],
+ [ 5.3,  5.1,  5.4,  5.7],
+ [ 9.6,  9.4,  7.8,  8.5],
+ [11.6, 12.6, 11.5, 12.8],
+ [12.2, 13.4, 12.3, 13.4]])
 
 model = tflearn.DNN(network())
 model.load('/content/electricity/save/model.tflearn')
