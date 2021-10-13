6b162d64723d03877984504f023072cf54b4fc6f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-13 21:42:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from nn.nets import network
 tf.debugging.set_log_device_placement(True)
 
 # Load model------------------------------------------------------------------------------------
-def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=10, batch_size=32, show_metric=True, name_saver=None):
+def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=400, batch_size=32, show_metric=True, name_saver=None):
   model = tflearn.DNN(network(), tensorboard_dir='/content/electricity/save/tflearn_logs')
   model.fit(data, labels, n_epoch=num_epochs, batch_size=batch_size, show_metric=show_metric, validation_set=(val_data, val_labels))
   model.save('/content/electricity/save/'+name_saver)
