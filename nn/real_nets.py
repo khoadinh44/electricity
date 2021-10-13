23c3307e221091f4efd5f81fe70b1ce79e9dc662
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-13 15:54:10 +0700

    Update real_nets.py

--- nn/real_nets.py
+++ nn/real_nets.py
@@ -1,11 +1,17 @@
 import tflearn
+import tensorflow as tf
 from utils.optimizers import rmsprop, adam, adagrad, momentum
+
+def leaky_relu(z, name=None):
+  return tf.maximum(0.01 * z, z, name=name)
 
 def network():
   # Build neural network
   net = tflearn.input_data(shape=[None, 4])
+  net = tflearn.fully_connected(net, 128)
+  net = leaky_relu(net)
   net = tflearn.fully_connected(net, 64)
-#   net = tflearn.activations.relu(net)
+  net = leaky_relu(net)
   net = tflearn.fully_connected(net, 32)
   net = tflearn.fully_connected(net, 4, activation='softmax')
   net = tflearn.regression(net, optimizer=adam, loss='categorical_crossentropy')
