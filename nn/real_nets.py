4661b876a5c8f9930d6419039e40c15ec09499fd
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-14 16:53:35 +0700

    Update real_nets.py

--- nn/real_nets.py
+++ nn/real_nets.py
@@ -1,18 +1,27 @@
 import tflearn
-import tensorflow as tf
 from utils.optimizers import rmsprop, adam, adagrad, momentum
-def leaky_relu(z, name=None):
-  return tf.maximum(0.01 * z, z, name=name)
-
-loss_obj = tf.keras.losses.CategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
+import keras
+import tensorflow as tf
 
 def network():
   # Build neural network
-  net = tflearn.input_data(shape=[None, 200]) 
-  net = tflearn.fully_connected(net, 150)
-  net = tf.keras.layers.PReLU()(net)
-  net = tflearn.fully_connected(net, 100)
-  net = tflearn.fully_connected(net, 4)
-  # net = tf.keras.layers.PReLU()(net)
-  net = tflearn.regression(net, optimizer=adam, loss='categorical_crossentropy')
-  return net
+  input_ = keras.layers.Input(shape=[4,])
+  hidden1 = keras.layers.Dense(30, activation=tf.keras.layers.PReLU())(input_)
+  hidden2 = keras.layers.Dense(30, activation=tf.keras.layers.PReLU())(hidden1)
+  concat = keras.layers.concatenate([input_, hidden2])
+  output = keras.layers.Dense(4, activation=None)(concat)
+  model = keras.models.Model(inputs=[input_], outputs=[output])
+  return model
+
+# def network():
+#   input_A = keras.layers.Input(shape=[2,], name="wide_input")
+#   input_B = keras.layers.Input(shape=[2,], name="deep_input")
+#   hidden1 = keras.layers.Dense(30, activation=tf.keras.layers.PReLU())(input_B)
+#   hidden2 = keras.layers.Dense(30, activation=tf.keras.layers.PReLU())(hidden1)
+#   concat = keras.layers.concatenate([input_A, hidden2])
+#   output = keras.layers.Dense(4, name="main_output")(concat)
+#   aux_output = keras.layers.Dense(4, name="aux_output")(hidden2)
+#   model = keras.models.Model(inputs=[input_A, input_B],
+#                             outputs=[output, aux_output])
+#   return model
+# tf.keras.layers.PReLU()
