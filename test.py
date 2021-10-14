af3817f0e0225df26afcc71e3f90d7be3d5ac493
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-14 16:01:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,15 @@ import tensorflow as tf
 import tflearn
 from nn.nets import network
 import numpy as np
+import keras
+from utils.data_loader import validation_examples, validation_targets
+from sklearn.metrics import precision_score
 
-print('\nEnter WSpeed: ')
-x=float(input())
-inputs=np.array([[1, x]])
+# print('\nEnter WSpeed: ')
+# x=float(input())
+# inputs=np.array([[1, x]])
 
-model = tflearn.DNN(network())
-model.load('/content/electricity/save/model.tflearn')
-y_pred = model.predict(inputs)
-print(f'\nThe prediction power is {y_pred[0][1]}')
+model = keras.models.load_model("/content/electricity/save/model.h5")
+y_pred = model.predict(validation_examples)
+print(f'\nThe prediction power is {y_pred[:10]}\n')
+print(validation_targets[:10])
