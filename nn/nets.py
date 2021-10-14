c12d0991f61813d05375176fffda704c103acbc2
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-14 16:00:44 +0700

    Update nets.py

--- nn/nets.py
+++ nn/nets.py
@@ -1,12 +1,13 @@
 import tflearn
 from utils.optimizers import rmsprop, adam, adagrad, momentum
+import keras
 
 def network():
   # Build neural network
-  net = tflearn.input_data(shape=[None, 2])
-  net = tflearn.fully_connected(net, 64)
-  net = tflearn.activations.relu(net)
-  net = tflearn.fully_connected(net, 32)
-  net = tflearn.fully_connected(net, 2, activation='softmax')
-  net = tflearn.regression(net, optimizer=adam, loss='categorical_crossentropy')
-  return net
+  input_ = keras.layers.Input(shape=[2,])
+  hidden1 = keras.layers.Dense(30, activation="relu")(input_)
+  hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+  concat = keras.layers.concatenate([input_, hidden2])
+  output = keras.layers.Dense(1)(concat)
+  model = keras.models.Model(inputs=[input_], outputs=[output])
+  return model
