a025884670f2f79e496bcbb5f1ad5ab290831814
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-14 16:54:03 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -2,22 +2,19 @@ import tensorflow as tf
 import tflearn
 from nn.real_nets import network
 import numpy as np
+import keras
+from utils.data_real_loader import validation_examples, validation_targets
 
-print('\nEnter WSpeed: ')
-inputs = np.array([[ 7.9,  8.9,  8.9,  9.3],
- [10.6, 11.5,  7.8, 10.7],
- [ 3.9,  4.6,  3.9,  3.6],
- [ 2.,   1.9,  1.7,  0.8],
- [11.3, 12.6, 10.2, 11.7],
- [ 2.9,  2.5,  1.5,  1.9],
- [ 5.3,  5.1,  5.4,  5.7],
- [ 9.6,  9.4,  7.8,  8.5],
- [11.6, 12.6, 11.5, 12.8],
- [12.2, 13.4, 12.3, 13.4]])
-one_val = np.ones((inputs.shape[0], 196))
-validation_examples = np.concatenate((inputs, one_val), axis=1).astype(np.float32)
+# print('\nEnter WSpeed: ')
+# x=float(input())
+# inputs=np.array([[1, x]])
 
-model = tflearn.DNN(network())
-model.load('/content/electricity/save/model.tflearn')
+model = keras.models.load_model("/content/electricity/save/model.h5")
 y_pred = model.predict(validation_examples)
-print(f'\nThe prediction power is {y_pred}')
+print(f'\nThe prediction power is {y_pred[:10]}\n')
+print(validation_targets[:10])
+# model = keras.models.load_model("/content/electricity/save/model.h5")
+# y_pred = model.predict([validation_examples[:, :2], validation_examples[:, 2:]])
+# print(f'\nThe prediction power is {y_pred[:10]}\n')
+# print(validation_targets[:10])
+
