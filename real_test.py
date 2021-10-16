437f99743967e6f7468a380a24304cb7e34073f6
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-16 16:20:51 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -5,16 +5,9 @@ import numpy as np
 import keras
 from utils.data_real_loader import validation_examples, validation_targets
 
-# print('\nEnter WSpeed: ')
-# x=float(input())
-# inputs=np.array([[1, x]])
-
-model = keras.models.load_model("/content/electricity/save/model.h5")
+model = keras.models.load_model("/content/electricity/save/model_5_tuabin.h5")
 y_pred = model.predict(validation_examples)
+
 print(f'\nThe prediction power is {y_pred[:10]}\n')
 print(validation_targets[:10])
-# model = keras.models.load_model("/content/electricity/save/model.h5")
-# y_pred = model.predict([validation_examples[:, :2], validation_examples[:, 2:]])
-# print(f'\nThe prediction power is {y_pred[:10]}\n')
-# print(validation_targets[:10])
 
