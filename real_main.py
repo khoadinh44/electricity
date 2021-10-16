3554c941fa211368e7cb4fc1d0314263e447d2c3
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-16 16:20:17 +0700

    Update real_main.py

--- real_main.py
+++ real_main.py
@@ -2,17 +2,18 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import tflearn
-
 from utils.data_real_loader import training_examples, training_targets, validation_examples, validation_targets
 from nn.real_nets import network
-# active GPU
+
+# active GPU--------------------------------------------------------------------------------------------------------------------------
 tf.debugging.set_log_device_placement(True)
 
+# parameter-----------------------------------------------------------------------------------------------------------------------------
 num_epochs = 4100
 batch_size = 32
+path_saver = '/content/drive/Shareddrives/newpro112233/electricity/weights/'
 
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
-
 def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=num_epochs, batch_size=batch_size, show_metric=True, name_saver=None):
   model = network()
   model.compile(loss="mean_squared_error",
@@ -22,17 +23,6 @@ def train(data=None, labels=None, val_data=None, val_labels=None, network=networ
   history = model.fit(data, labels, epochs=num_epochs,
                      validation_data=(val_data, val_labels),
                      callbacks=[callback])
-  model.save('/content/drive/Shareddrives/newpro112233/electricity/weights/'+name_saver)
+  model.save(path_saver+name_saver)
 
-# def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=num_epochs, batch_size=batch_size, show_metric=True, name_saver=None):
-#   model = network()
-#   model.compile(loss=["binary_crossentropy", "binary_crossentropy"],
-#                 metrics=[tf.keras.metrics.Precision()],
-#                 optimizer=tf.keras.optimizers.Adam(),
-#                 loss_weights=[0.5, 0.5])
-
-#   history = model.fit([data[:, :2], data[:, 2:]], [labels, labels], epochs=num_epochs,
-#                     validation_data=([val_data[:, :2], val_data[:, 2:]], [val_labels, val_labels]))
-#   model.save('/content/electricity/save/'+name_saver)
-
-train(training_examples, training_targets, validation_examples, validation_targets, network=network, name_saver='model_5_tb_2.h5')
+train(training_examples, training_targets, validation_examples, validation_targets, network, num_epochs, batch_size, False, name_saver='model_5_tuabin.h5')
