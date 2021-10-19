8708e7b47c8a429220f341efe40abdd12b6dd3be
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-19 10:06:20 +0700

    Update real_main.py

--- real_main.py
+++ real_main.py
@@ -4,17 +4,23 @@ import numpy as np
 import tflearn
 from utils.data_real_loader import training_examples, training_targets, validation_examples, validation_targets
 from nn.real_nets import network
+from datetime import datetime
+import keras
 
 # active GPU--------------------------------------------------------------------------------------------------------------------------
 tf.debugging.set_log_device_placement(True)
 
 # parameter-----------------------------------------------------------------------------------------------------------------------------
-num_epochs = 4100
+num_epochs = 8000
 batch_size = 32
 path_saver = '/content/drive/Shareddrives/newpro112233/electricity/weights/'
 name_saver = 'model_5_tuabin.h5'
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+# Define the Keras TensorBoard callback.
+logdir="/content/drive/Shareddrives/newpro112233/electricity/logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
+# callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=num_epochs, batch_size=batch_size, show_metric=True, name_saver=None):
   model = network()
   model.compile(loss="mean_squared_error",
@@ -23,7 +29,7 @@ def train(data=None, labels=None, val_data=None, val_labels=None, network=networ
 
   history = model.fit(data, labels, epochs=num_epochs,
                      validation_data=(val_data, val_labels),
-                     callbacks=[callback])
+                     callbacks=[tensorboard_callback])
   model.save(path_saver+name_saver)
 
 train(training_examples, training_targets, validation_examples, validation_targets, network, num_epochs, batch_size, True, 'model_5_tuabin.h5')
