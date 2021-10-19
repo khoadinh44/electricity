097966e87f665c631c299b3db430573fb49cb375
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-19 10:38:20 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -8,6 +8,6 @@ from utils.data_real_loader import validation_examples, validation_targets
 model = keras.models.load_model("/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin.h5")
 y_pred = model.predict(validation_examples)
 
-print(f'\nThe prediction power is {y_pred[:10]}\n')
+print(f'\nThe prediction powers are \n{y_pred[:10]}\n')
+print('\nThe true powers are:\n')
 print(validation_targets[:10])
-
