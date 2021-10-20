49327ad9d64030154988c8f7151bd0549880f51e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-20 11:58:23 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -3,10 +3,13 @@ from nn.real_nets import network
 import numpy as np
 import keras
 from utils.data_real_loader import validation_examples, validation_targets
-from real_main import compensation
+from keras.models import load_model
 
-model = network().load_weights("/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin_next.h5")
-y_pred = np.array(model.predict(validation_examples)) - compensation
+model = network()
+model.summary()
+model.load_weights('/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin_next.h5') 
+
+y_pred = np.array(model.predict(validation_examples.astype(np.float32))) - 71.7
 
 print(f'\nThe prediction powers are \n{y_pred[:10]}\n')
 print('\nThe true powers are:\n')
