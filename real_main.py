cd66cf397fc2a4bc270195ac0e1969b7bccb027c
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-20 10:49:17 +0700

    Update real_main.py

--- real_main.py
+++ real_main.py
@@ -6,15 +6,16 @@ from utils.data_real_loader import training_examples, training_targets, validati
 from nn.real_nets import network
 from datetime import datetime
 import keras
+from angular_grad import AngularGrad
 
 # active GPU--------------------------------------------------------------------------------------------------------------------------
 tf.debugging.set_log_device_placement(True)
 
 # parameter-----------------------------------------------------------------------------------------------------------------------------
-num_epochs = 6000
+num_epochs = 10000
 batch_size = 32
 path_saver = '/content/drive/Shareddrives/newpro112233/electricity/weights/'
-name_saver = 'model_5_tuabin_16.h5'
+name_saver = 'model_5_tuabin_next.h5'
 
 # Define the Keras TensorBoard callback.
 logdir="/content/drive/Shareddrives/newpro112233/electricity/logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -23,9 +24,10 @@ tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 # callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 def train(data=None, labels=None, val_data=None, val_labels=None, network=network, num_epochs=num_epochs, batch_size=batch_size, show_metric=True, name_saver=None):
   model = network()
+  # model.load_weights('/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin.h5')
   model.compile(loss="mean_squared_error",
                 metrics=[tf.keras.metrics.Precision()],
-                optimizer=tf.keras.optimizers.Adam())
+                optimizer=AngularGrad())
 
   history = model.fit(data, labels, epochs=num_epochs,
                      validation_data=(val_data, val_labels),
