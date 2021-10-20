bec9a3a0127d5de220a3a71da1706afe1a471f7e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-10-20 16:07:22 +0700

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 from nn.real_nets import network
 import numpy as np
 import keras
-from utils.data_real_loader import validation_examples, validation_targets
+from utils.data_real_loader import validation_examples, validation_targets, min_targets, max_targets
 from keras.models import load_model
 
 model = network()
 model.summary()
-model.load_weights('/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin_next.h5') 
+model.load_weights('/content/drive/Shareddrives/newpro112233/electricity/weights/model_5_tuabin_next_0_1.h5') 
 
-y_pred = np.array(model.predict(validation_examples.astype(np.float32))) - 71.7
+y_pred = (np.array(model.predict(validation_examples))*(max_targets+min_targets)) - min_targets
 
 print(f'\nThe prediction powers are \n{y_pred[:10]}\n')
 print('\nThe true powers are:\n')
-print(validation_targets[:10] - 71.7)
+print(validation_targets[:10]*(max_targets+min_targets) - min_targets)
